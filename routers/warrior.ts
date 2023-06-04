4d65342d40c43e09af120de99f53549f01799614
grzeg <grzegorzterenda@gmail.com>
2023-06-04 09:27:35 +0200

    fished adding warrior to DB by warrior Router

--- routers/warrior.ts
+++ routers/warrior.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import {WarriorRecord} from "../records/warrior.record";
 
 
 export const warriorRouter = Router();
@@ -7,8 +8,16 @@ warriorRouter
     .get('/add-form', async (req, res) => {
         res.render('warrior/add-form.hbs');
     })
-    .post('/', async (req, res) => {
-        const warrior = await req.body
-        console.log(warrior)
+    .post('/',  async (req, res) => {
+        //req.body returns string, so it is necessary to convert stats to Number
+        const warrior = new WarriorRecord({
+            ...req.body,
+            power: Number(req.body.power),
+            defence: Number(req.body.defence),
+            stamina: Number(req.body.stamina),
+            agility: Number(req.body.agility),
+            name: req.body.name,
+        });
+        await warrior.insert();
         res.render('warrior/warrior-added.hbs');
     });
