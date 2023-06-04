ac08a5c6b35e54cb4b9e93b306c8298118792f50
grzeg <grzegorzterenda@gmail.com>
2023-06-04 17:56:10 +0200

    created warrior1 and warrior 2 by destructuring body + validation of warriors

--- routers/arena.ts
+++ routers/arena.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import {WarriorRecord} from "../records/warrior.record";
+import {ValidationError} from "../utils/error";
 
 export const arenaRouter = Router();
 
@@ -10,6 +11,19 @@ arenaRouter
             warriors,
         });
     })
-    .post('/fight', (req, res) => {
+    .post('/fight', async (req, res) => {
+        const {warrior1: warrior1Id, warrior2: warrior2Id} = req.body
+        if(warrior1Id === warrior2Id) {
+            throw new ValidationError ('A warrior cannot fight himself. Choose another fighter.');
+        }
+
+        const warrior1 = await WarriorRecord.getOne(warrior1Id);
+        const warrior2 = await WarriorRecord.getOne(warrior2Id);
         res.render('./arena/fight');
+        if(!warrior1) {
+            throw new ValidationError('First warrior  not found.')
+        }
+        if(!warrior2) {
+            throw new ValidationError('Second warrior not found.')
+        }
     }) //POST/ arena/ fight
