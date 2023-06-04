dd9d07a950378fbb1c7ac26a6c9ff617e4bd4bad
grzeg <grzegorzterenda@gmail.com>
2023-06-04 17:24:26 +0200

    fixed Hall of Fame view

--- routers/hall-of-fame.ts
+++ routers/hall-of-fame.ts
@@ -1,13 +1,21 @@
-import { Router } from "express"
+import {Router} from "express"
 import {WarriorRecord} from "../records/warrior.record";
 
 export const hallOfFameRouter = Router();
 
 hallOfFameRouter
     .get('/', async (req, res) => {
-        const hallOfFameList = await WarriorRecord.listTop(10);
+        const hallOfFameList = (await WarriorRecord.listTop(10)).map((warrior, index) => {
+                return {
+                    place: index + 1,
+                    warrior
+
+                }
+            }
+        );
         console.log(hallOfFameList);
+
         res.render('./hall-of-fame/list.hbs', {
             hallOfFameList
-        })
-    })
+        });
+    });
